68a9c0a824994c6f97e946932dc0393aec318108
Gabriela Sato <gabisato.ssb@gmail.com>
2018-12-01 17:33:34 -0300

    criptografando mensagens

--- Sockets/src/sockets/udp/UDPClient.java
+++ Sockets/src/sockets/udp/UDPClient.java
@@ -1,5 +1,8 @@
 package sockets.udp;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
@@ -7,6 +10,10 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPClient {
+
+	static String IV = "AAAAAAAAAAAAAAAA";
+	static String chaveencriptacao = "0123456789abcdef";
+
 	public static void main(String args[]) throws Exception {
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
@@ -17,7 +24,8 @@ public class UDPClient {
 			System.out.println("Cliente preparado para enviar: ");
 			//Lê entrada do usuário
 			String sentence = inFromUser.readLine();
-			sendData = sentence.getBytes();
+			//criptografa a mensagem
+            		sendData = encrypt(sentence, chaveencriptacao);
 			//Cria pacote udp
 			DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length, IPAddress, 9876);
 			//envia ao servidor
@@ -25,9 +33,41 @@ public class UDPClient {
 			//Recebe resposta do servidor
 			DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
 			clientSocket.receive(receivePacket);
-			String modifiedSentence = new String(receivePacket.getData());
+			//descriptografa a mensagem de resposta
+            		String modifiedSentence = decrypt(mensagemCriptografada(receiveData), chaveencriptacao);
 			System.out.println("Recebido do servidor UDP:" + modifiedSentence);
 			//Fecha conexão: clientSocket.close();
 		}
 	}
+
+    	public static byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+        	Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+        	SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+        	encripta.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+        	return encripta.doFinal(textopuro.getBytes("UTF-8"));
+    	}
+
+    	public static String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception{
+        	Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+        	SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+        	decripta.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+        	return new String(decripta.doFinal(textoencriptado),"UTF-8");
+    	}
+
+    	public static byte[] mensagemCriptografada(byte[] receiveData){
+        	int contador = 0;
+
+        	for(int i=0; i<receiveData.length; i++){
+        	    if(receiveData[i] != 0){
+        	        contador++;
+        	    }
+        	}
+
+        	byte[] textoEncriptado = new byte[contador];
+
+        	for (int i=0; i<textoEncriptado.length; i++){
+        	    textoEncriptado[i] = receiveData[i];
+        	}
+        	return textoEncriptado;
+    	}
 }
--- Sockets/src/sockets/udp/UDPServer.java
+++ Sockets/src/sockets/udp/UDPServer.java
@@ -1,10 +1,17 @@
 package sockets.udp;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPServer {
+
+        static String IV = "AAAAAAAAAAAAAAAA";
+	static String chaveencriptacao = "0123456789abcdef";
+
 	public static void main(String args[]) throws Exception {
 		//Cria um servidor UDP na porta 9876
 		DatagramSocket serverSocket = new DatagramSocket(9876);
@@ -16,13 +23,15 @@ public class UDPServer {
 			//Recebe as mensagens dos clientes
 			DatagramPacket receivePacket = new DatagramPacket(receiveData,	receiveData.length);
 			serverSocket.receive(receivePacket);
-			String sentence = new String(receivePacket.getData());
+                        //descriptografa a mensagem
+                        String sentence = decrypt(mensagemCriptografada(receiveData), chaveencriptacao);
 			System.out.println("Recebido: " + sentence);
 			//Responde ao mesmo IP e Porta do pacote recebido.
 			InetAddress IPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
 			String capitalizedSentence = sentence.toUpperCase();
-			sendData = capitalizedSentence.getBytes();
+                        //criptografa a mensagem de resposta
+                        sendData = encrypt(capitalizedSentence, chaveencriptacao);
 			DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length, IPAddress, port);
 			serverSocket.send(sendPacket);
 			
@@ -30,4 +39,35 @@ public class UDPServer {
 				receiveData[i] = 0;
 		}
 	}
+        public static byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+                Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+                SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+                encripta.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+                return encripta.doFinal(textopuro.getBytes("UTF-8"));
+        }
+
+        public static String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception{
+               Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+               SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+               decripta.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+               return new String(decripta.doFinal(textoencriptado),"UTF-8");
+        }
+
+        //Tentativa de resolver o problema do preenchimento do vetor receiveData com 0 nos índices restantes aos da mensagem
+        public static byte[] mensagemCriptografada(byte[] receiveData){
+               int contador = 0;
+
+               for(int i=0; i<receiveData.length; i++){
+               		if(receiveData[i] != 0){
+                       		contador++;
+               		}
+               }
+
+               byte[] textoEncriptado = new byte[contador];
+
+               for (int i=0; i<textoEncriptado.length; i++){
+               		textoEncriptado[i] = receiveData[i];
+               }
+               return textoEncriptado;
+    }
 }
